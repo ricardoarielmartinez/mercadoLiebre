169acbeea93617c0defd9ebb47e8297145e9dd00
ricardoarielmartinez <ricardoarielmartinez@gmail.com>
2022-10-25 21:42:37 -0300

    inicio el proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000,()=>{
+    console.log("Servidor iniciado");
+})
+
+
+app.get("/",(req,res)=>{
+    res.sendFile (path.resolve("./views/home.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile (path.resolve("./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile (path.resolve("./views/login.html"))
+})

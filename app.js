3f673fd11a02f82bb25a100b81c8061e32a9d8d3
ricardoarielmartinez <ricardoarielmartinez@gmail.com>
2022-10-25 21:49:26 -0300

    se modifica app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3001,()=>{
     console.log("Servidor iniciado");
 })
 
